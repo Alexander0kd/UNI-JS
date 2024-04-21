238a8888c33af7c8eb8ea9d7c60069c81f24757e
Alexander Kademskyi <109386355+Alexander0kd@users.noreply.github.com>
2024-04-21 22:29:27 +0300

    Add files via upload

---
+++ lab7/scripts.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const BASE_URL = 'https://raw.githubusercontent.com/Alexander0kd/UNI-JS/main/lab7';
+
+    const bindHeaderButtons = () => {
+        const home = document.getElementById('btn-home');
+        if (home) {
+            home.addEventListener('click', () => {
+                loadHome();
+            });
+        }
+
+        const catalog = document.getElementById('btn-catalog');
+        if (catalog) {
+            home.addEventListener('click', () => {
+                loadCatalog();
+            });
+        }
+    };
+
+
+    // Header
+    const loadHome = async () => {
+        const page = await ajax.GET(`${BASE_URL}/components/home.html`, true);
+
+        console.log(page);
+    };
+
+    const loadCatalog = async () => {
+        const page = await ajax.GET(`${BASE_URL}/components/catalog.html`, true);
+        const catalog = await ajax.GET(`${BASE_URL}/data/categories/response.json`, false);
+
+        console.log(page);
+        console.log(catalog);
+
+        console.log("Catalog:");
+        console.log(JSON.parse(catalog));
+        console.log(JSON.stringify(catalog));
+    };
+
+    // Home
+    const loadRandom = async () => {
+        const data = await ajax.GET(`${BASE_URL}/data/categories/response.json`, false);
+
+        console.log(data);
+    };
+
+    // Catalog
+    const loadCatalogGroup = async (catalogId) => {
+        const page = await ajax.GET(`${BASE_URL}/components/group.html`, true);
+        const catalog = await ajax.GET(`${BASE_URL}/data/categories/response.json`, false);
+
+        console.log(page);
+        console.log(catalog);
+    };
+
+    // Group
+    const loadItem = async (catalogId, itemId) => {
+        const page = await ajax.GET(`${BASE_URL}/components/item.html`, true);
+        const catalog = await ajax.GET(`${BASE_URL}/data/categories/response.json`, false);
+        
+        console.log(page);
+        console.log(catalog);
+    };
+
+    bindHeaderButtons();
+});
